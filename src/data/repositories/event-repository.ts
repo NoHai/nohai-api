c7fe2ae26be2a8c1973b61b3d26b906f1dc34633
Radu B <radu.buciuceanu@gmail.com>
2019-01-30 02:09:13 +0200

    [feature-5] Data automapper was integrated in event repository.

--- src/data/repositories/event-repository.ts
+++ src/data/repositories/event-repository.ts
@@ -3,23 +3,32 @@ import { map, switchMap } from 'rxjs/operators';
 import { Event as EventModel } from '../../business/models/event';
 import { EventsParameter } from '../../business/models/parameters/events-parameter';
 import { IEventRepository } from '../../business/repositories/i-event-repository';
+import { IDataAutomapper } from '../../data-mapping/i-data-automapper';
 import { Event as EventEntity } from '../entities/event';
 import { IStorage } from '../i-storage';
 import { IStorageFactory } from '../i-storage-factory';
 
 export class EventRepository implements IEventRepository {
     private readonly storage: Observable<IStorage<EventEntity>>;
+    private readonly mapper: IDataAutomapper;
 
-    constructor(storageFactory: IStorageFactory) {
+    constructor(storageFactory: IStorageFactory, mapper: IDataAutomapper) {
         this.storage = storageFactory.make<EventEntity>();
+        this.mapper = mapper;
     }
 
     insert(event: EventModel): Observable<EventModel> {
-        return this.storage.pipe(map((storage) => storage.insert(event)));
+        return this.mapper
+            .map<EventModel, EventEntity>(event)
+            .pipe(switchMap((eventEntity) => this.insertEntity(eventEntity)))
+            .pipe(switchMap((eventEntity) => this.mapper.map<EventEntity, EventModel>(eventEntity)));
     }
 
     getMany(filter: EventsParameter): Observable<EventModel[]> {
         return this.storage.pipe(switchMap((storage) => storage.getMany(filter)));
+    }
 
+    private insertEntity(eventEntity: EventEntity): Observable<EventEntity> {
+        return this.storage.pipe(switchMap((storage) => storage.insert(eventEntity)));
     }
 }
