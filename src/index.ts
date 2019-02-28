85d025c4734c22b564460ad7ce56a291cb1bff69
Radu B <radu.buciuceanu@gmail.com>
2019-02-28 11:24:01 +0200

    [feature-44] Environments were refactored.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
-import express from 'express';
 import 'reflect-metadata';
-import { tap } from 'rxjs/operators';
-import { CreateContainer } from './presentation/commands/ioc/create-container';
-import { ResolveService } from './presentation/commands/ioc/resolve-service';
+import {StartupFactory} from './presentation/commands/startup/startup-factory';
 
-const port: number = 5000;
-const expressInstance = express();
-const resolveService = new ResolveService(new CreateContainer().execute());
-
-expressInstance.listen(port, async () => {
-    resolveService.execute('initializeDatabaseConnection')
-        .execute()
-        .pipe(tap((_) => console.log('NoHai application started.')))
-        .subscribe();
-});
-
-expressInstance.get('/', async (_, response: any) => {
-    response.send('NoHai application.');
-});
+const startupFactory = new StartupFactory();
+startupFactory.make().execute();
---
+++ src/presentation/commands/ioc/i-create-container.ts
@@ -0,0 +1,6 @@
+import {AwilixContainer} from 'awilix';
+import {ICommand} from '../../../business/commands/core/i-command';
+import {Nothing} from '../../../business/models/nothing';
+
+export interface ICreateContainer extends ICommand<Nothing, AwilixContainer> {
+}
