ba2804614360dc0a1d34a33eb0de40e6522e003e
Radu B <radu.buciuceanu@gmail.com>
2019-03-16 11:41:45 +0200

    [feature-49] startup classes were refactored.

---
+++ src/presentation/commands/startup/startup-common.ts
@@ -0,0 +1,45 @@
+﻿import { Observable } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+import { Nothing } from '../../../business/models/nothing';
+import { IInitializeDatabaseConnection } from '../../../data/commands/i-initialize-database-connection';
+import { IPresentationSettings } from '../../i-presentation-settings';
+import { IInitializeGraph } from '../graph/i-initialize-graph';
+import { CreateProductionContainer } from '../ioc/create-production-container';
+import { ResolveService } from '../ioc/resolve-service';
+import { IStartup } from './i-startup';
+
+export abstract class StartupCommon implements IStartup {
+    protected readonly express: any;
+    protected readonly resolveService = new ResolveService(new CreateProductionContainer().execute());
+    protected readonly presentationSettings: IPresentationSettings;
+    protected readonly initializeDatabase: IInitializeDatabaseConnection;
+    protected readonly initializeGraph: IInitializeGraph;
+
+    constructor() {
+        this.express = this.resolveService.execute('express');
+        this.presentationSettings = this.resolveService.execute('presentationSettings');
+        this.initializeDatabase = this.resolveService.execute('initializeDatabaseConnection');
+        this.initializeGraph = this.resolveService.execute('initializeGraph');
+    }
+
+    execute(): Observable<Nothing> {
+        return this.initializeDatabase.execute()
+            .pipe(switchMap(() => this.initializeGraph.execute()))
+            .pipe(tap(() => this.listen()))
+            .pipe(tap(() => this.get()));
+    }
+
+    private listen(): void {
+        this.express.listen(this.presentationSettings.port, () => {
+            console.log(this.listenLog);
+        });
+    }
+
+    private get(): void {
+        this.express.get('/', (_: any, response: any) => {
+            response.send('NoHai application.');
+        });
+    }
+
+    protected abstract get listenLog(): string;
+}
--- src/presentation/commands/startup/startup-production.ts
+++ src/presentation/commands/startup/startup-production.ts
@@ -1,43 +1,7 @@
-import { Observable } from 'rxjs';
-import { switchMap, tap } from 'rxjs/operators';
-import { Nothing } from '../../../business/models/nothing';
-import { IInitializeDatabaseConnection } from '../../../data/commands/i-initialize-database-connection';
-import { IPresentationSettings } from '../../i-presentation-settings';
-import { IInitializeGraph } from '../graph/i-initialize-graph';
-import { CreateProductionContainer } from '../ioc/create-production-container';
-import { ResolveService } from '../ioc/resolve-service';
-import { IStartup } from './i-startup';
+import { StartupCommon } from './startup-common';
 
-export class StartupProduction implements IStartup {
-    private readonly express: any;
-    private readonly resolveService = new ResolveService(new CreateProductionContainer().execute());
-    private readonly presentationSettings: IPresentationSettings;
-    private readonly initializeDatabase: IInitializeDatabaseConnection;
-    private readonly initializeGraph: IInitializeGraph;
-
-    constructor() {
-        this.express = this.resolveService.execute('express');
-        this.presentationSettings = this.resolveService.execute('presentationSettings');
-        this.initializeDatabase = this.resolveService.execute('initializeDatabaseConnection');
-        this.initializeGraph = this.resolveService.execute('initializeGraph');
-    }
-
-    execute(): Observable<Nothing> {
-        return this.initializeDatabase.execute()
-            .pipe(switchMap(() => this.initializeGraph.execute()))
-            .pipe(tap(() => this.listen()))
-            .pipe(tap(() => this.get()));
-    }
-
-    private listen(): void {
-        this.express.listen(this.presentationSettings.port, () => {
-            console.log('NoHai application started on production environment.');
-        });
-    }
-
-    private get(): void {
-        this.express.get('/', (_: any, response: any) => {
-            response.send('NoHai application.');
-        });
+export class StartupProduction extends StartupCommon {
+    protected get listenLog(): string {
+        return 'NoHai application started on production environment.';
     }
 }
