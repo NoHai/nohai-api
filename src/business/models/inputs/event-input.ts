cdcd5e9b8fe49ed5dec9bc73fa22f2fb38072b03
Andreea Stanchi <andreea.stanchi89@gmail.com>
2019-05-04 13:15:03 +0300

    [feature-22] added new fields to event

--- src/business/models/inputs/event-input.ts
+++ src/business/models/inputs/event-input.ts
@@ -1,5 +1,10 @@
-﻿export class EventInput {
+export class EventInput {
     title!: string;
+    description!: string;
+    location!: any;
+    sport!: any;
+    participantsNumber!: number;
+    cost!: number;
 
     constructor(init?: Partial<EventInput>) {
         Object.assign(this, init);
--- src/business/models/results/event.ts
+++ src/business/models/results/event.ts
@@ -1,5 +1,10 @@
 ﻿export class Event {
     title!: string;
+    description!: string;
+    location!: any;
+    sport!: any;
+    participantsNumber!: number;
+    cost!: number;
 
     constructor(init?: Partial<Event>) {
         Object.assign(this, init);
