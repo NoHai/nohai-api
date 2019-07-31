3a3869f2154d1fed2ed62726feb1fffc547a531c
Andreea Stanchi <andreea.stanchi@edelcode.com>
2019-07-31 14:58:27 +0300

    added id to credentials as needed in frontend

--- src/business/models/results/credentials.ts
+++ src/business/models/results/credentials.ts
@@ -1,4 +1,10 @@
 import { CredentialsInput } from '../inputs/credentials-input';
 
 export class Credentials extends CredentialsInput {
+    id!: string;
+
+    constructor(init?: any) {
+        super(init);
+        Object.assign(this, init);
+    }
 }
