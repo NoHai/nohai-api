095c4a8aa157a5dd1e20f372480e1cda3bba46a8
Andreea Stanchi <andreea.stanchi@edelcode.com>
2019-08-03 18:13:31 +0300

    [feature-53] add sport node

--- src/business/commands/get-sports.ts
+++ src/business/commands/get-sports.ts
@@ -1,4 +1,5 @@
 import { Observable } from 'rxjs';
+import { toArray } from 'rxjs/operators';
 import { Sport } from '../models/results/sport';
 import { ISportRepository } from '../repositories/i-sport-repository';
 import { IGetSports } from './i-get-sports';
@@ -8,7 +9,8 @@ export class GetSports implements IGetSports {
     constructor(private sportRepository: ISportRepository) {
     }
 
-    execute(): Observable<Sport> {
-        return this.sportRepository.getAll();
+    execute(): Observable<Sport[]> {
+        return this.sportRepository.getAll()
+            .pipe(toArray());
     }
 }
--- src/business/commands/i-get-sports.ts
+++ src/business/commands/i-get-sports.ts
@@ -1,5 +1,5 @@
 import { Sport } from '../models/results/sport';
 import { IObservableCommand } from './core/i-observable-command';
 
-export interface IGetSports extends IObservableCommand<null, Sport> {
+export interface IGetSports extends IObservableCommand<null, Sport[]> {
 }
