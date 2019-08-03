f66226fbcfd11775a7d47703c3c53f8d0d331451
Radu B <radu.buciuceanu@gmail.com>
2019-08-03 17:16:40 +0300

    [feature-53] Fix process.env.EVIRONMENT.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { resolve } from 'path';
 import 'reflect-metadata';
 import { StartupFactory } from './presentation/commands/startup/startup-factory';
 
-dotenv.config({ path: resolve(__dirname, `../.env.${process.env.ENVIRONMENT}`) });
+dotenv.config({ path: resolve(__dirname, `../.env.${process.env.environment}`) });
+
 const startupFactory = new StartupFactory();
 startupFactory.make().execute().subscribe();
 
