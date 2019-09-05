c476fcf0ad7cbec8640ca9020be9ccce830f3223
Andreea Stanchi <andreea.stanchi@edelcode.com>
2019-09-05 15:00:51 +0300

    [feature-60] add authHelper

--- src/business/commands/create-user.ts
+++ src/business/commands/create-user.ts
@@ -3,19 +3,17 @@ import { CredentialsInput } from '../models/inputs/credentials-input';
 import { Credentials } from '../models/results/credentials';
 import { IUserRepository } from '../repositories/i-user-repository';
 import { ICreateUser } from './i-create-user';
-import * as bcrypt from 'bcrypt';
+
 import { flatMap } from 'rxjs/operators';
-import { Errors } from '../utilities/Errors';
+import { Errors } from '../../utilities/Errors';
+import { AuthHelper } from '../../utilities/auth-helper';
 
 export class CreateUser implements ICreateUser {
     constructor(private userRepository: IUserRepository) {
     }
 
     execute(input: CredentialsInput): Observable<Credentials> {
-        const salt = bcrypt.genSaltSync(10);
-        const hash =  bcrypt.hashSync(input.password, salt);
-        const hashedInput = new CredentialsInput({ password: hash, login: input.login });
-
+        const hashedInput = AuthHelper.hashCredentials(input);
         return this.userRepository.byCredentials(input)
                     .pipe(flatMap((user) => iif(() => user !== undefined)
                                 ? throwError(Errors.AlreadyRegisterdError)
---
+++ src/utilities/auth-helper.ts
@@ -0,0 +1,47 @@
+import { sign, verify, decode } from 'jsonwebtoken';
+import * as bcrypt from 'bcrypt';
+import { CredentialsInput } from '../business/models/inputs/credentials-input';
+import { Observable, of, from } from 'rxjs';
+import uuid = require('uuid');
+
+export class AuthHelper {
+
+    static  signToken(accessToken: any): string {
+        return sign(accessToken, process.env.NOHAI_JWT_SECRET || '', {
+            algorithm: 'HS256',
+            encoding: 'UTF8',
+        });
+    }
+
+    static hashCredentials(input: CredentialsInput): CredentialsInput {
+        const salt = bcrypt.genSaltSync(10);
+        const hash =  bcrypt.hashSync(input.password, salt);
+
+        return new CredentialsInput({ password: hash, login: input.login });
+    }
+
+    static verifyToken(token: string): boolean {
+        try {
+            const payload = verify(token, process.env.NOHAI_JWT_SECRET || '');
+            console.log(payload);
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    static decodeToken(token: string) {
+        return decode(token);
+    }
+
+    static buildRefreshToken(): Observable<string> {
+        return of(uuid().toString());
+    }
+
+     static comparePassords(inputPassword: string, credentials: any): Observable<boolean> {
+         return from(bcrypt.compare(inputPassword, credentials.password));
+    }
+
+}
+
