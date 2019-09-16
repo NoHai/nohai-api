f0d6d0d148ce2b136842c4e6b3f19b1222fb0105
Andreea Stanchi <andreea.stanchi@edelcode.com>
2019-09-16 11:31:54 +0300

    login fix

--- src/business/commands/create-user.ts
+++ src/business/commands/create-user.ts
@@ -1,12 +1,11 @@
-import { Observable, iif, throwError } from 'rxjs';
+import { Observable, throwError, empty, iif, never, of } from 'rxjs';
 import { CredentialsInput } from '../models/inputs/credentials-input';
 import { Credentials } from '../models/results/credentials';
 import { IUserRepository } from '../repositories/i-user-repository';
 import { ICreateUser } from './i-create-user';
-
-import { flatMap } from 'rxjs/operators';
-import { Errors } from '../../utilities/Errors';
+import { flatMap, catchError, tap } from 'rxjs/operators';
 import { AuthHelper } from '../../utilities/auth-helper';
+import { Errors } from '../../utilities/Errors';
 
 export class CreateUser implements ICreateUser {
     constructor(private userRepository: IUserRepository) {
@@ -14,9 +13,12 @@ export class CreateUser implements ICreateUser {
 
     execute(input: CredentialsInput): Observable<Credentials> {
         const hashedInput = AuthHelper.hashCredentials(input);
+
         return this.userRepository.byCredentials(input.login)
-                    .pipe(flatMap((user) => iif(() => user !== undefined)
-                                ? throwError(Errors.AlreadyRegisterdError)
-                                :  this.userRepository.insert(hashedInput)));
+            .pipe(catchError(() => of(undefined)))
+            .pipe(flatMap((user) => iif(() => user !== undefined,
+                throwError(Errors.AlreadyRegisterdError),
+                this.userRepository.insert(hashedInput))));
+
     }
 }
