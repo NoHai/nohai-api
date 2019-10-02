d9d5ce951a1a99a8c4c2734c7a1ce76030caab32
Danut Ilie <daniel_ilie97@yahoo.com>
2019-10-02 11:09:59 +0300

    try to fix firebase private key

--- src/services/service-account.ts
+++ src/services/service-account.ts
@@ -1,10 +1,17 @@
 export class ServiceAccount {
   static getFirebabaseConfig(): any {
+    const isDevelopment = process.env.environment === 'development';
+    const firebasePrivateKey = isDevelopment
+      ? process.env.NOHAI_FIREBASE_PRIVATE_KEY
+      : JSON.parse(process.env.NOHAI_FIREBASE_PRIVATE_KEY as string);
+
+    console.log(firebasePrivateKey);
+
     return {
       type: 'service_account',
       project_id: 'nohai-pushnotification',
       private_key_id: process.env.NOHAI_FIREBASE_PRIVATE_KEY_ID,
-      private_key: process.env.NOHAI_FIREBASE_PRIVATE_KEY as string,
+      private_key: firebasePrivateKey,
       client_email: process.env.NOHAI_FIREBASE_CLIENT_EMAIL,
       client_id: process.env.NOHAI_FIREBASE_CLIENT_ID,
       auth_uri: 'https://accounts.google.com/o/oauth2/auth',
