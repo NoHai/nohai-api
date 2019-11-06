6f27e88b7027f554d49ef473f64bb85d23576c61
Andreea Stanchi <andreea.stanchi@edelcode.com>
2019-11-06 14:45:16 +0200

    [feature-100] - remove correct the notification

--- src/business/commands/cancel-pending-request.ts
+++ src/business/commands/cancel-pending-request.ts
@@ -3,23 +3,25 @@ import { Observable, iif, of } from 'rxjs';
 import { INotificationRepository } from '../repositories/i-notification-repository';
 import { IUserEventsRepository } from '../repositories/i-user-events-repository';
 import { UserContext } from '../../utilities/user-context';
-import { NotificationType } from '../../data/enums/notification-type';
 import { flatMap, catchError } from 'rxjs/operators';
+import { IEventRepository } from '../repositories/i-event-repository';
 
 export class CancelPendingRequest implements ICancelPendingRequest {
 
     constructor(private readonly notificationRepository: INotificationRepository,
+                private readonly eventRepository: IEventRepository,
                 private readonly userEventsRepository: IUserEventsRepository,
                 private readonly userContext: UserContext) {
 
     }
 
     execute(parameter: string): Observable<boolean> {
-        return this.notificationRepository.delete({
-            eventId: parameter,
-            userId: this.userContext.userId,
-            type: NotificationType.JoinRequest,
-        })
+        return this.eventRepository.getById(parameter)
+            .pipe(flatMap((event) => this.notificationRepository.delete({
+                eventId: parameter,
+                userId: event.owner.id,
+                createdUser: this.userContext.userId,
+            })))
             .pipe(flatMap(() => this.userEventsRepository.delete({ eventId: parameter, userId: this.userContext.userId })))
             .pipe(flatMap((deleteResult) => iif(() => deleteResult !== undefined && deleteResult > 0, of(true), of(false))))
             .pipe(catchError(() => of(false)));
