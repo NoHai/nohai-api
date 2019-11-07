31d3886e9b6fef71d3aa5f57367f4264b86dd561
Andreea Stanchi <andreea.stanchi@edelcode.com>
2019-11-07 10:35:49 +0200

    [feature-103] - the edit of the event should also notify users by email

--- src/business/commands/update-event.ts
+++ src/business/commands/update-event.ts
@@ -1,40 +1,72 @@
-import { Observable, from, zip } from 'rxjs';
+import { Observable, from, zip, of, iif } from 'rxjs';
 import { UpdateEventInput } from '../models/inputs/update-event-input';
 import { Event as EventResult } from '../models/results/event';
 import { IEventRepository } from '../repositories/i-event-repository';
 import { IUpdateEvent } from './i-update-event';
-import { map, flatMap, filter } from 'rxjs/operators';
+import { map, flatMap, filter, catchError } from 'rxjs/operators';
 import { IUserEventsRepository } from '../repositories/i-user-events-repository';
 import { NotificationHelper } from '../../utilities/notification-helper';
 import { Notification } from '../../data/entities/notification';
 import { NotificationToken } from '../models/results/notification-token';
 import { INotificationTokenRepository } from '../repositories/i-notification-token-repository';
 import { In } from 'typeorm';
+import { UserEventsStatus } from '../../data/enums/user-events-status';
+import { IUserRepository } from '../repositories/i-user-repository';
+import { EmailHelper } from '../../utilities/email-helper';
+import { EmailService } from '../../services/email-service';
 
 export class UpdateEvent implements IUpdateEvent {
     constructor(private eventRepository: IEventRepository,
                 private readonly userEventsRepository: IUserEventsRepository,
-                private readonly notificationTokenRepository: INotificationTokenRepository) {
+                private readonly userRepository: IUserRepository,
+                private readonly notificationTokenRepository: INotificationTokenRepository, 
+                private readonly emailService: EmailService) {
     }
 
     execute(input: UpdateEventInput): Observable<EventResult> {
         const updateEventFlow = this.eventRepository.update(input);
-        const userIdsFlow = this.userEventsRepository.find({ eventId: input.id })
-            .pipe(map((userEvents) => userEvents.map((u) => u.userId)));
+        const sendNotificationFlow = this.sendNotifications(input);
+        const sendEmails = this.sendEmails(input);
 
-        const notificationTokenFlow = this.userEventsRepository.find({ eventId: input.id})
-            .pipe(map((users) => users.map((u) => u.userId)))
-            .pipe(filter((userIds) => userIds && userIds.length > 0))
-            .pipe(flatMap((ids) =>  this.notificationTokenRepository.find({ where: { userId: In(ids)} })));
-
-        return zip(updateEventFlow, userIdsFlow, notificationTokenFlow)
+        return zip(updateEventFlow, sendNotificationFlow, sendEmails)
             .pipe(map((result) => {
-                const notifications = NotificationHelper.buildCancelEventNotifications(input, result[1]);
-                notifications.map((notification) => {
-                    const tokens = result[2].filter( (n) => n.userId === notification.userId);
-                    this.sendNotification(notification, tokens); });
                 return result[0];
             }));
+    }
+
+
+    private sendNotifications(event: UpdateEventInput): Observable<boolean> {
+        const userIdsFlow = this.userEventsRepository.find({ eventId: event.id, status: UserEventsStatus.Approved })
+        .pipe(map((userEvents) => userEvents.map((u) => u.userId)));
+
+        const notificationTokenFlow = this.userEventsRepository.find({ eventId: event.id, status: UserEventsStatus.Approved})
+        .pipe(map((users) => users.map((u) => u.userId)))
+        .pipe(filter((userIds) => userIds && userIds.length > 0))
+        .pipe(flatMap((ids) =>  this.notificationTokenRepository.find({ where: { userId: In(ids)} })));
+
+        return zip(userIdsFlow, notificationTokenFlow)
+            .pipe(map((result) => {
+                const notifications = NotificationHelper.buildEditEventNotifications(event, result[0]);
+                notifications.map((notification) => {
+                    const tokens = result[1].filter( (n) => n.userId === notification.userId);
+                    this.sendNotification(notification, tokens); });
+                return true;
+            }))
+            .pipe(catchError(() => of(false)));
+    }
+
+    private sendEmails(event: UpdateEventInput): Observable<boolean> {
+        return  this.userEventsRepository.find({ eventId: event.id, status: UserEventsStatus.Approved })
+            .pipe(map((userEvents) => userEvents.map((u) => u.userId)))
+            .pipe(flatMap((userIds) => this.userRepository.getWithCredentials(userIds)))
+            .pipe(flatMap((users) => {
+               const emails =  EmailHelper.getEditEventEmails(users, event.title);
+               return this.emailService.sendMultipleEmails(emails);
+            }))
+            .pipe(flatMap((emailResult) => iif(() => !!emailResult && emailResult[0].statusCode === 202,
+                                    of(true),
+                                    of(false))));
+
     }
 
     private sendNotification(notification: Notification, tokens: NotificationToken[]) {
--- src/utilities/email-helper.ts
+++ src/utilities/email-helper.ts
@@ -70,6 +70,21 @@ export class EmailHelper {
         });
     }
 
+    static getEditEventEmails(users: any[], eventTitle: string): Email[] {
+        const emails = users.map((user) => {
+            return new Email({
+                to: user.email,
+                from: process.env.NOHAI_CUSTOMER_SERVICE_EMAIL,
+                subject: 'Modificare eveniment',
+                text: this.editEventMessage(eventTitle),
+                html: this.getEditEmailHtml(user.user, eventTitle),
+            });
+
+        });
+
+        return emails;
+    }
+
     static recoverPasswordSuccessfully(email: string): Observable<string> {
         return of(`Link-ul de recuperare parola s-a trimis la adresa ${email} si este valid o zi.`);
     }
@@ -97,6 +112,10 @@ export class EmailHelper {
 
     private static kickoutUserMessage(eventTitle: string): string {
         return `Ne pare rau, dar administratorul evenimentului ${eventTitle}, te-a eliminat`;
+    }
+
+    private static editEventMessage(eventTitle: string): string {
+        return `Evenimentul ${eventTitle} a fost modificat.`;
     }
 
     private static getRecoveryEmailHtml(user: User, link: string): string {
@@ -201,6 +220,27 @@ export class EmailHelper {
             <mj-text font-family="helvetica">Ne pare rau sa te anuntam dar administratorul evenimentului
                  ${eventTitle} te-a retras din activitate.</mj-text>
             <mj-text font-family="helvetica">Te asteptam la urmatoarele evenimente.</mj-text>
+            <mj-text font-family="helvetica">Iti multumim</mj-text>
+            </mj-column>
+        </mj-section>
+        <mj-section>
+        <mj-column>
+            <mj-text  font-family="helvetica">Echipa NoHai</mj-text>
+        </mj-column>
+        </mj-section>
+        </mj-body>
+        </mjml>
+        `).html;
+    }
+
+    private static getEditEmailHtml(user: User, eventTitle: string): string {
+        return mjml2html(`
+        <mjml>
+        <mj-body>p
+        <mj-section>
+            <mj-column>
+            <mj-text font-family="helvetica">Salut ${user.firstName} ${user.lastName},</mj-text>
+            <mj-text font-family="helvetica">Detaliile evenimentului ${eventTitle} au fost modificate.</mj-text>
             <mj-text font-family="helvetica">Iti multumim</mj-text>
             </mj-column>
         </mj-section>
